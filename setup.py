54ae6172bf2d7aaeeb0a923893e1080aa9943bb0
Илья Киров <kirov@bars-open.ru>
2018-04-26 17:50:55 +0300

    Убрал пока builder

--- setup.py
+++ setup.py
@@ -48,10 +48,10 @@ setup(
         'Development Status :: 5 - Production/Stable',
     ),
     dependency_links=(
-        'http://pypi.bars-open.ru/simple/m3-builder',
+        #'http://pypi.bars-open.ru/simple/m3-builder',
     ),
     setup_requires=(
-        'm3-builder>=1.0.1',
+        #'m3-builder>=1.0.1',
     ),
     set_build_info=os.path.dirname(__file__),
 )
