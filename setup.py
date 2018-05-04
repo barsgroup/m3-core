563941a9bc721359faa7e7a591a7982c4eb65ed0
Илья Киров <kirov@bars-open.ru>
2018-05-04 13:25:19 +0300

    TDDN5-4 Перенес зависимости из файла в setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,7 @@
 # coding: utf-8
 from __future__ import absolute_import
 import os
-
-from pip.download import PipSession
-from pip.req.req_file import parse_requirements
 from setuptools import setup, find_packages
-
-
-def _get_requirements(file_name):
-    pip_session = PipSession()
-    requirements = parse_requirements(file_name, session=pip_session)
-
-    return tuple(str(requirement.req) for requirement in requirements)
 
 
 def _read(fname):
@@ -20,6 +10,7 @@ def _read(fname):
             fname)).read()
     except IOError:
         return ''
+
 
 setup(
     name='m3-core',
@@ -30,7 +21,10 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages('src'),
     description=_read('DESCRIPTION.md'),
-    install_requires=_get_requirements('REQUIREMENTS'),
+    install_requires=(
+        'm3-builder>=1.0.1',
+        'm3-django-compat>=1.2.3',
+    ),
     long_description=_read('README.md'),
     include_package_data=True,
     classifiers=(
