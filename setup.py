7d659b0a388f7121569c3bc6192fd2f9da5390cd
Дмитрий Чернышов <dechernyshov@bars-open.ru>
2020-03-17 10:44:08 +0300

    Убран dependency_links

--- setup.py
+++ setup.py
@@ -39,9 +39,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Development Status :: 5 - Production/Stable',
     ],
-    dependency_links=(
-        'http://pypi.bars-open.ru/simple/m3-builder',
-    ),
     setup_requires=(
         'm3-builder>=1.2.0',
     ),
